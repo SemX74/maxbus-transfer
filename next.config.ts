4f454377aa43567157b6603b5c9856101594dc3c
SemX74 <semka147semov@gmail.com>
2025-07-05 18:49:10 +0300

    fix build

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,14 @@ const withNextIntl = createNextIntlPlugin(
 );
 
 const nextConfig: NextConfig = {
+  experimental: {
+    esmExternals: "loose",
+  },
+  images: {
+    formats: ["image/webp", "image/avif"],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
